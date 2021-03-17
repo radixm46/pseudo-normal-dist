f6be6b55c8b83ea1952ce00aad912e9d76ac317b
radixm46 <radixm46@gmail.com>
2021-03-17 14:57:18 +0900

    init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// generate random numbers
+// call this from main.html
+
+function main {
+  // make 13 integers sum 100
+  const listLength = 100;
+  var distributionList = [];
+  // use for loop, generate 12 len bool tuple
+  // loop via iterator?
+  return true;
+
+}
---
+++ src/test/test.js
@@ -0,0 +1,7 @@
+// random numbers assertion
+
+// assert numbers are int
+
+// assert sum of 13 int as 100
+
+// assert distribution?
