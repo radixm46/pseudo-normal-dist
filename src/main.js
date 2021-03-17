0dbc97517f22dff75eae0820ac26e1d82dbc32e8
radixm46 <radixm46@gmail.com>
2021-03-17 17:01:47 +0900

    working on main function

--- src/main.js
+++ src/main.js
@@ -4,9 +4,21 @@
 function main {
   // make 13 integers sum 100
   const listLength = 100;
-  var distributionList = [];
+  const numberOfGroups = 13;
+  var distributionList = new Int8Array(listLength);
+
+  // enter loop of distributionList
   // use for loop, generate 12 len bool tuple
-  // loop via iterator?
+  for (const int8elem in distributionList) {
+    //hoge;
+    var additionalVals = new Int8Array(numberOfGroups);
+    for (const addelem in additionalVals) {
+      // generate list of bool, use for additional value
+      // use random here
+    }
+    // add sum of additionalVals to distributionlist
+  }
   return true;
+  // count elements
 
 }
