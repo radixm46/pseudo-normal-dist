a2784c112e31e774ca0d0c9ce49be623059a5915
radixm46 <radixm46@gmail.com>
2021-03-19 02:25:51 +0900

    beta version

--- src/main.js
+++ src/main.js
@@ -1,24 +1,46 @@
 // generate random numbers
 // call this from main.html
 
-function main {
-  // make 13 integers sum 100
-  const listLength = 100;
-  const numberOfGroups = 13;
+const generateRandomNums = (listLength = 100, numberOfGroups = 13, offset = 0.0) => {
   var distributionList = new Int8Array(listLength);
 
-  // enter loop of distributionList
-  // use for loop, generate 12 len bool tuple
-  for (const int8elem in distributionList) {
-    //hoge;
-    var additionalVals = new Int8Array(numberOfGroups);
-    for (const addelem in additionalVals) {
-      // generate list of bool, use for additional value
-      // use random here
+  distributionList.forEach((val, index) => {
+      val = 0;
+      for (const addelem in new Int8Array(numberOfGroups)) {
+        val += Math.round(Math.random() + offset);
+      }
+      distributionList[index] = val;
     }
-    // add sum of additionalVals to distributionlist
-  }
-  return true;
-  // count elements
-
+  );
+  return [distributionList.sort(), numberOfGroups];
 }
+
+
+const generateFreqTable = (array, numOfGroups) => {
+  // given array must be sorted (ascending)
+  let freqResult = new Int8Array(numOfGroups);
+  for (let i = 0; i < numOfGroups; i++) {
+    const result = array.filter(num => num === i);
+    freqResult[i] = result.length;
+  }
+  return freqResult;
+}
+
+
+const plotStuff = (idx, srcArray, idxchar='-') => {
+  let pltTitle = 'pattern'+(idx+1);
+  splitBarTop = pltTitle + '+'.repeat(20 - pltTitle.length) + '<br>';
+  splitBarBot = '+'.repeat(20) + '<br>';
+  let returnStr = '';
+  for (const arrNum of srcArray) {
+    returnStr += idxchar.repeat(arrNum) + `:${arrNum}<br>`;
+  }
+  return splitBarTop + returnStr + splitBarBot;
+}
+
+// test
+//generateRandomNums(100, 13)
+//console.log(generateRandomNums(100, 13));
+//console.log(
+//  ...generateFreqTable(...generateRandomNums(100, 13, -0.2))
+//);
