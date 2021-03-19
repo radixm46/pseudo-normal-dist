d79a29ee33731d9a4ea00811f278d75e8e490763
radixm46 <radixm46@gmail.com>
2021-03-19 16:15:41 +0900

    update function as class, reconstruct layout

--- src/main.js
+++ src/main.js
@@ -1,48 +1,46 @@
 // generate random numbers
 // call this from main.html
 
-// generate rondom numbers with
-// n = listLength, number range = numberOfGroups, median? offset = offset
-const generateRandomNums = (listLength = 100, numberOfGroups = 13, offset = 0.0) => {
-  const distributionList = new Int16Array(listLength);
+class RandomNormalGenerator {
+  // generate rondom numbers with
+  // n = listLength, number range = numberOfGroups, median? offset = offset
+  constructor(listLength = 100, numberOfGroups = 13, offset = 0.0) {
+    this.distributionList= new Int16Array(listLength);
+    this.numberOfGroups = numberOfGroups;
+    this.offset = offset;
+    this.distributionList.forEach((val, index) => {
+      val = 0;
+      for (const addelem in new Int16Array(numberOfGroups)) {
+        val += Math.round(Math.random() + offset);
+      }
+      this.distributionList[index] = val;
+    });
+    this.distributionList = this.distributionList.sort();
 
-  distributionList.forEach((val, index) => {
-    val = 0;
-    for (const addelem in new Int16Array(numberOfGroups)) {
-      val += Math.round(Math.random() + offset);
+    // generate frequency table from given list obj
+    const genFreqArr = () => {
+      const freqArray = new Int16Array(this.numberOfGroups);
+      // given array must be sorted (ascending)
+      for (let i = 0; i < this.numberOfGroups; i++) {
+        const result = this.distributionList.filter(num => num === i);
+        freqArray[i] = result.length;
+      }
+      return freqArray;
+    };
+    this.freqArray = genFreqArr();
+  }
+
+  // generate simple ascii bar plot
+  plotStuff(idx, idxchar = "=") {
+    const pltTitle = "pattern" + (idx + 1);
+    //let splitBarTop =  `<p>${"+".repeat(5)}<b>${pltTitle}</b>`+ "+".repeat(20 - pltTitle.length) + "<br>";
+    //let splitBarBot = "+".repeat(25) + "</p>";
+    //let freqArrayStr = `<b>${pltTitle}: </b>${this.freqArray}<br>${"-".repeat(25)}<br>`;
+    let freqArrayStr = `<h2>${pltTitle}: ${this.freqArray}</h2>`;
+    let returnStr = "";
+    for (const arrNum of this.freqArray) {
+      returnStr += idxchar.repeat(arrNum) + `:${arrNum}<br>`;
     }
-    distributionList[index] = val;
+    return freqArrayStr + `<p>${returnStr}</p>`;
   }
-  );
-  return [distributionList.sort(), numberOfGroups];
-};
-
-// generate frequency table from given list obj
-const generateFreqTable = (array, numOfGroups) => {
-  // given array must be sorted (ascending)
-  const freqResult = new Int16Array(numOfGroups);
-  for (let i = 0; i < numOfGroups; i++) {
-    const result = array.filter(num => num === i);
-    freqResult[i] = result.length;
-  }
-  return freqResult;
-};
-
-// generate simple ascii bar plot
-const plotStuff = (idx, srcArray, idxchar = "=") => {
-  const pltTitle = "pattern" + (idx + 1);
-  splitBarTop =  `<p>${"+".repeat(5)}<b>${pltTitle}</b>`+ "+".repeat(20 - pltTitle.length) + "<br>";
-  splitBarBot = "+".repeat(25) + "</p>";
-  let returnStr = "";
-  for (const arrNum of srcArray) {
-    returnStr += idxchar.repeat(arrNum) + `:${arrNum}<br>`;
-  }
-  return splitBarTop + returnStr + splitBarBot;
-};
-
-// test
-// generateRandomNums(100, 13)
-// console.log(generateRandomNums(100, 13));
-// console.log(
-//  ...generateFreqTable(...generateRandomNums(100, 13, -0.2))
-// );
+}
